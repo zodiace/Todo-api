e31248ced72407af0d2812a2cf2e8ed9f5a884a9
Matt Lewis <matthew@matix.me>
2016-01-01 22:15:41 -0400

    Added PUT function

--- server.js
+++ server.js
@@ -63,6 +63,36 @@ app.delete ('/todos/:id', function (req, res) {
 	}
 });
 
+app.put ('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = req.body;
+	body = _.pick(body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	}
+	else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	}
+	else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(matchedTodo, validAttributes);
+
+	res.json(matchedTodo);
+})
+
 app.listen(PORT, function () {
 	console.log('Express server started, listening on port ' + PORT + '...');
 });

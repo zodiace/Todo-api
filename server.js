163cb3a3ce885a93ef8d0dc7de6cdacd4b1bcea7
Matt Lewis <matthew@matix.me>
2016-01-02 13:20:58 -0400

    Updated to support SQL on GET/todo/id

--- server.js
+++ server.js
@@ -39,15 +39,25 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send('"message": "No matching item"');
+		}
+	}, function (e) {
+		res.status(404).send(e.toJSON());
+	});
+	// var matchedTodo = _.findWhere(todos, {
+	// 	id: todoId
+	// });
+
+	// if (matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 });
 
 app.post('/todos', function(req, res) {

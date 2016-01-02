f4cb0d6d56ab452176318525ad9a08e4016a15ef
Matt Lewis <matthew@matix.me>
2016-01-02 08:21:43 -0400

    Added ?completed filter

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.get('/', function (req, res) {
 });
 
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(todos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(todos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function (req, res) {

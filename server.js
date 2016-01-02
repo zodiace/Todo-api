e5b0534eb8906f18f60ce2d25d91783d39f32593
Matt Lewis <matthew@matix.me>
2016-01-02 08:38:05 -0400

    Added description filtering

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/todos', function (req, res) {
 		filteredTodos = _.where(todos, {completed: false});
 	}
 
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		})
+	}
+
 	res.json(filteredTodos);
 });
 

49591d346ecd2a402894c6782e7f40b4964ccee0
Matt Lewis <matthew@matix.me>
2016-01-02 21:38:36 -0400

    Updated POST/ for sequelize

--- server.js
+++ server.js
@@ -72,18 +72,32 @@ app.post('/todos', function(req, res) {
 
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else {
-		res.status(404).json({
-			"error": "No todo found with id " + id
-		});
-	}
+	db.todo.destroy({
+		where: {
+		id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				"error": "No todo with id"
+			})
+		} else {
+			res.status(204).send();
+		}
+	}, function () {
+		res.status(500).send();
+	})
+
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			return todo.destroy();
+		} else {
+			res.status(404).send('"message": "No matching item"');
+		}
+	}).then(function (todo) {
+		res.json(todo);
+	});
 });
 
 app.put('/todos/:id', function(req, res) {

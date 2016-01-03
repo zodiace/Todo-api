736e4c3893cc42a31744b683be7d529119cd43f8
Matt Lewis <matthew@matix.me>
2016-01-03 08:56:57 -0400

    Added hash and salt password

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.post('/users', function(req, res) {
 	body = _.pick(body, 'email', 'password');
 
 	db.user.create(body).then(function (user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function (e) {
 		res.status(404).json(e);
 	});
